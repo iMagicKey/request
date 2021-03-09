594507494eaf413474142a9a1c64c33589d2fedc
root <root@vps-fd32aded.vps.ovh.net>
2021-03-09 14:04:06 +0300

    Node 15+ Update

--- index.js
+++ index.js
@@ -1,105 +1,74 @@
-const https = require('https');
-const http = require('http');
-const zlib = require('zlib');
-const URL = require('url');
+const https = require('https')
+const http = require('http')
+const zlib = require('zlib')
 
 function Request(url, options = {}) {
     let requestOptions = {
         headers: {},
         ...options,
-        ...URL.parse(url)
     }
-
-    let requester = requestOptions.protocol == 'https:' ? https : http;
+ 
+    let requester = url.indexOf('https:') != -1 ? https : http
 
     return new Promise((resolve, reject) => {
-        var body = '';
+        let buffers = []
+        
+        let request = requester.request(url, requestOptions, (res) => {
 
-        var request = requester.request(requestOptions, (res) => {
-
-            var output;
+            let output
             switch (res.headers['content-encoding']) {
                 case 'br':
-                    var br = zlib.createBrotliDecompress();
-                    res.pipe(br);
-                    output = br;
+                    let br = zlib.createBrotliDecompress()
+                    res.pipe(br)
+                    output = br
 
-                    break;
+                    break
                 
                 case 'gzip':
-                    var gzip = zlib.createGunzip();
-                    output = zlib.createGunzip();
-                    res.pipe(gzip);
-                    output = gzip;
+                    let gzip = zlib.createGunzip()
+                    output = zlib.createGunzip()
+                    res.pipe(gzip)
+                    output = gzip
 
-                    break;
+                    break
 
                 case 'deflate':
-                    var deflate = zlib.createInflate();
-                    res.pipe(deflate);
-                    output = deflate;
+                    let deflate = zlib.createInflate()
+                    res.pipe(deflate)
+                    output = deflate
 
-                    break;
+                    break
 
                 default:
-                    output = res;
-                    break;
+                    output = res
+                    break
             }
 
             output.on('data', (data) => {
-                data = data.toString('utf-8');
-                body += data;
-                if (requestOptions.abortAfter) {
-
-                    if (body.length >= requestOptions.abortAfter) {
-                        request.abort();
-
-                        return resolve({
-                            ...res,
-                            body: body
-                        });
-                    }
-                }
-            });
+                buffers.push(Buffer.from(data, 'binary'))
+            })
 
             output.on('end', () => {
-                if (res.statusCode == 301 || res.statusCode == 302) {
-                    if (res.headers.location) {
-                        if (res.headers['set-cookie']) {
-                            requestOptions.headers.cookie = res.headers['set-cookie'];
-                        }
-
-                        return resolve(Request(res.headers.location, requestOptions));
-                    }
-                }
-
-                if (res.statusCode < 200 || res.statusCode >= 300) {
-                    return reject({
-                        ...res,
-                        body: body
-                    })
-                }
-
                 return resolve({
                     ...res,
-                    body: body
-                });
-            });
+                    buffer: Buffer.concat(buffers)
+                })
+            })
         })
 
         request.on('error', (err) => {
             return reject({
                 ...err,
-                body: body
-            });
-        });
+                buffer: Buffer.concat(buffers)
+            })
+        })
 
         if (requestOptions.body) {
-            request.write(requestOptions.body);
+            request.write(requestOptions.body)
         }
 
-        request.end();
+        request.end()
     })
 }
 
-module.exports = Request;
+module.exports = Request
